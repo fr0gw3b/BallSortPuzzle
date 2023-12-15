68974527ce4b75f7cee53d7ccfb50699effdd90a
Suwiii221 <153897724+Suwiii221@users.noreply.github.com>
2023-12-15 09:58:46 +0100

    Create mdl.py

---
+++ mdl.py
@@ -0,0 +1,19 @@
+from random import randint
+class Balls:
+  def __init__(self,col,dim):
+    self.color = col
+    self.dimension = dim
+  def couleur(self):
+   # 1 = couleur_1rouge 
+    #2 = couleur_2 bleu
+    #3 = couleur_3 vert
+    #4 = couleur_4 jaune
+    
+
+
+
+ 
+  def dimension_ball(self,dime):
+
+
+  
