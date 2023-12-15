13792387f81c7fac0b32b703d9ab9abad0253625
KoyDox <153897471+KoyDox@users.noreply.github.com>
2023-12-15 09:31:19 +0100

    Update Tube.py

--- Modules/Tube.py
+++ Modules/Tube.py
@@ -1,8 +1,25 @@
-# Module Tube
+class Pile :
 
-class Tube:
-    def __init__(self):
-        pass
+def __init__(self, x):
+    self.elmts = []
+    self.maxi=x
 
-    def est_vide(self):
-        pass
+def est_vide(self):
+    return self.elmts == []
+
+def empiler (self, x):
+    if self.est_plein():
+        raise IndexError ("La pile est plaine")
+    else :
+        self.elmts.append(x)
+
+def depiler (self):
+    if self.est_vide()
+        raise IndexError ("La pile est vide")
+    return self.elmts.pop()
+
+def est_plein(self):
+    return len(self.elmts) == self.maxi
+
+def new_maxi(self, x):
+    self.maxi = x
