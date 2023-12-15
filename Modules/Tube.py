0515c4d68371de48bb22f352f8769c25fdb4cbd9
Fr0g <26202334+fr0gw3b@users.noreply.github.com>
2023-12-15 09:32:48 +0100

    Update Tube.py

--- Modules/Tube.py
+++ Modules/Tube.py
@@ -1,25 +1,24 @@
 class Pile :
+    def __init__(self, x):
+        self.elmts = []
+        self.maxi = x
 
-def __init__(self, x):
-    self.elmts = []
-    self.maxi=x
+    def est_vide(self):
+        return self.elmts == []
 
-def est_vide(self):
-    return self.elmts == []
+    def empiler (self, x):
+        if self.est_plein():
+            raise IndexError ("La pile est plaine")
+        else :
+            self.elmts.append(x)
 
-def empiler (self, x):
-    if self.est_plein():
-        raise IndexError ("La pile est plaine")
-    else :
-        self.elmts.append(x)
+    def depiler (self):
+        if self.est_vide():
+            raise IndexError ("La pile est vide")
+        return self.elmts.pop()
 
-def depiler (self):
-    if self.est_vide()
-        raise IndexError ("La pile est vide")
-    return self.elmts.pop()
+    def est_plein(self):
+        return len(self.elmts) == self.maxi
 
-def est_plein(self):
-    return len(self.elmts) == self.maxi
-
-def new_maxi(self, x):
-    self.maxi = x
+    def new_maxi(self, x):
+        self.maxi = x
