0467852b89b64e9f9a15dbafb86735d112ae4643
Fr0g <26202334+fr0gw3b@users.noreply.github.com>
2023-12-15 09:24:58 +0100

    Update Niveaux.py

--- Modules/Niveaux.py
+++ Modules/Niveaux.py
@@ -1,1 +1,16 @@
 # Module de niveaux
+    # Tom
+
+class Niveau:
+    def __init__(self):
+        self.niveau = 0
+        pass
+
+    def recuperer_niveau(self):
+        return self.niveau
+
+    def reinitialisation_niveau(self):
+        pass
+
+    def niveau_suivant(self):
+        pass
