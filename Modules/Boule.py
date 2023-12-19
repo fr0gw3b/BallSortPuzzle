8ed4aec1c232864479ff45fa36a2c980cf1296fa
Fr0g <26202334+fr0gw3b@users.noreply.github.com>
2023-12-19 15:27:35 +0100

    Update Boule.py

--- Modules/Boule.py
+++ Modules/Boule.py
@@ -1,1 +1,29 @@
-# Modules boules
+# Cody
+
+class Tube:
+    def __init__(self):
+        self.elmts = []
+        self.maxi = 4
+
+    def est_vide(self):
+        return self.elmts == []
+
+    def empiler(self, x):
+        if self.est_plein():
+            raise IndexError ("La pile est plaine")
+        else :
+            self.elmts.append(x)
+
+    def depiler (self):
+        if self.est_vide():
+            raise IndexError ("La pile est vide")
+        return self.elmts.pop()
+
+    def est_plein(self):
+        return len(self.elmts) == self.maxi
+
+    def new_maxi(self, x):
+        self.maxi = x
+
+    def afficher_elements(self):
+        return self.elmts
