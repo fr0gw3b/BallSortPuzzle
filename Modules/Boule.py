62d3db261d926d4e20d7e08613a7aea13bc7fb65
KoyDox <153897471+KoyDox@users.noreply.github.com>
2023-12-22 08:36:37 +0100

    Update Boule.py

--- Modules/Boule.py
+++ Modules/Boule.py
@@ -1,6 +1,17 @@
 class Boule:
     def __init__(self, couleur):
+        """
+        Initialise un objet Boule avec une couleur spécifiée.
+
+        Paramètres :
+        - couleur : Couleur de la boule.
+
+        La couleur de la boule est définie lors de l'initialisation de l'objet.
+        """
         self.couleur = couleur
 
     def get_couleur(self):
+        """
+        Renvoie la couleur de la boule.
+        """
         return self.couleur
